da10b247d3efd7f4024576e0e874f22fa76d6334
Morgane MAUREL <mormaurelpro@gmail.com>
2023-02-23 11:57:26 +0100

    Add React Router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -6,11 +7,18 @@ import { MantineProvider } from '@mantine/core';
 import MyTheme from './utils/myTheme';
 import './assets/styles/App.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
 function App() {
   return (
     <MantineProvider theme={MyTheme} withGlobalStyles withCSSVariables withNormalizeCSS>
       <Header />
-      <HomePage />
+      <RouterProvider router={router} />
       <Footer />
     </MantineProvider>
   );
