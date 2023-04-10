df9201dc67b4f4740b598b8c3a53102a64033436
Morgane MAUREL <mormaurelpro@gmail.com>
2023-04-10 10:55:02 +0200

    Try create logout

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -1,16 +1,38 @@
-import { Image, Text, Title, Box, useMantineTheme } from '@mantine/core';
+import { Image, Text, Title, Box, Button, useMantineTheme } from '@mantine/core';
 import { useContext, useState } from 'react';
 import { CurrentUserContext } from '../App';
 import { login } from '../utils/api';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
 
     const theme = useMantineTheme();
+    const navigate = useNavigate();
     const {user, setUser} = useContext(CurrentUserContext)
+    const logout = () => {
+        setUser({
+            firstName: '',
+            lastName: '',
+            email: '',
+        })
+        navigate('/home')
+    };
+
     console.log(user)
+
     return (
         <Box>
            <Text>Hello {user?.firstName} {user?.lastName} !</Text>
+           <Button 
+            // type="submit" 
+            className='button' 
+            color="dark" size="md" 
+            compact 
+            style={{ margin: 15 }}
+            onClick={logout}
+          >
+            Déconnexion
+          </Button> 
         </Box>
     )
 }
